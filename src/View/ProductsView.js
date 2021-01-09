c5275cb709e03e29037488aad3a7ef2432da3af4
Dima <dmytrotus@gmail.com>
2021-01-09 13:44:39 +0100

    refactoring

--- src/View/ProductsView.js
+++ src/View/ProductsView.js
@@ -3,12 +3,13 @@ import React from 'react';
 export function ProductsView(props){
 
 	const products = props.products;
+	const deletePdt = props.deletePdt;
 
 	return(
 		<ul>
 		{products.map(el => 
 			<li key={el.id}>{el.name} 
-				<button onClick={() => props.deletePdt(el.id)}>Удалить</button>
+				<button onClick={() => deletePdt(el.id)}>Удалить</button>
 			</li>
 		)}
 		</ul>
